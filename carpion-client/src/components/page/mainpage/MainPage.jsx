9f32cb145f31d457afe8b74cd24a1c4322300e64
chlsun <134248448+chlsun@users.noreply.github.com>
2025-04-15 16:33:55 +0900

    rentPage_ing

--- carpion-client/src/components/page/mainpage/MainPage.jsx
+++ carpion-client/src/components/page/mainpage/MainPage.jsx
@@ -2,15 +2,19 @@ import { useNavigate } from "react-router-dom";
 import "../reset.css";
 
 const MainPage = () => {
-    const navi = useNavigate();
+   const navi = useNavigate();
 
-    return (
-        <>
-            <h3 onClick={() => navi("/admin")} style={{ cursor: "pointer" }}>
-                운영자페이지 이동
-            </h3>
-        </>
-    );
+   return (
+      <>
+         <h3
+            className="test-navi"
+            onClick={() => navi("/rent")}
+            style={{ cursor: "pointer" }}
+         >
+            자동차 렌트 페이지 이동
+         </h3>
+      </>
+   );
 };
 
 export default MainPage;
